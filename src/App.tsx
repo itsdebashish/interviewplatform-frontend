f3016fb272c17a2723723bed206139e984b9e727
Debashish-C <debashish.sahu.official@gmail.com>
2026-07-05 16:57:35 +0530

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 function App() {
   return (
-    <div>
-        <h1>Interview Platform</h1>
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        AI Interview Platform
+      </h1>
     </div>
   )
 }
+
 
 export default App
