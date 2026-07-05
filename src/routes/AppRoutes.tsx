0fd83a9a18f0f7fcc2f07800466c21dd75b77b59
Debashish-C <debashish.sahu.official@gmail.com>
2026-07-05 18:20:27 +0530

    added layout navbar

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,15 +5,19 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import NotFound from "../pages/NotFound";
 
+import MainLayout from "../components/layout/MainLayout"
+
 
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
